7c47bb94df6dcb0962ee060de3116263370f07dd
Momin Rather <momin.rathe@avi.software>
2023-09-29 17:43:30 +0530

    dynamic updation testing

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -16,4 +16,26 @@ router.post("/registerDoctor", registerDoctor);
 router.get("/duplicate", detectDuplicate);
 router.post("/login", loginUser);
 
+router.get("/patients", async (req, res) => {
+  let obj = { fullname: "Momin", contact: "9876543211" };
+
+  let patient_id = "GUbTv_fimBW3nZBGXpebQ";
+  let query = "Update patients set ";
+
+  var keys = Object.keys(obj);
+  var last = keys[keys.length - 1];
+
+  for (let item in obj) {
+    query += item + "=" + `'${obj[item]}'`;
+    if (item !== last) query += ",";
+  }
+
+  query += ` where patient_id='GUbTv_fimBW3nZBGXpebQ'`;
+
+  console.log(query);
+  // let tablename = "patients";
+  const result = await pool.query(query);
+  res.json(result.rows);
+});
+
 module.exports = router;
